5594d6aacad5fddb05881c1bfee0e0edef65f5b9
Venom-61 <subhendukarmakar61@gmail.com>
2023-01-02 21:36:23 +0530

    Hossam and Friends

--- b.cpp
+++ b.cpp
@@ -3,10 +3,32 @@ using namespace std;
 
 typedef long long ll;
 
-// B. Hossam and Friends
+// B. Hassam and Friends
 
-void solve() {
-    
+void solve(int tt) {
+    int n, m;
+    cin >> n >> m;
+    vector<int> mn(n + 1);
+
+    for(int i = 1; i <= n; i++)
+        mn[i] = n;
+
+    for(int i = 0; i < m; ++i) {
+        int x, y;
+        cin >> x >> y;
+        if(x > y)
+            swap(x, y);
+        mn[x] = min(mn[x], y - 1);
+    }
+
+    for(int i = n-1; i; --i) 
+        mn[i] = min(mn[i], mn[i + 1]);
+
+    ll ans = n;
+    for(int i = 0; i < n; ++i)
+        ans += (mn[i] - i);
+
+    cout << ans << "\n";
 }
 
 int32_t main() {
@@ -16,7 +38,8 @@ int32_t main() {
     int test = 1;
     cin >> test;
     for(int i=1; i<=test; i++) {
-        solve();
+        solve(test);
+        cout << "\n";
     }
 
     return 0;
